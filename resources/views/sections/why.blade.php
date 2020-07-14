7b3d0e83ecc83d2951a78b31602d7433545732e7
Arthur Villar <=>
2020-07-14 13:45:35 -0400

    Add favicon image

--- resources/views/sections/why.blade.php
+++ resources/views/sections/why.blade.php
@@ -1,7 +1,7 @@
 <section id="why-us" class="mb-7">
 	<div class="container-fluid p-0">
 		<div class="row no-gutters">
-			<div class="col-lg-6 col-md-6 col-12">
+			<div class="col-lg-6 col-12">
 				<img src="{{asset('images/office.jpg')}}" class="w-100">
 				<div class="bg-primary p-6 d-flex text-white align-items-center">
 					<div class="mr-5">
@@ -10,7 +10,7 @@
 					<h4 class="highlight-bold m-0">It is a long established fact that a reader will be tracted by the readable content</h4>
 				</div>
 			</div>
-			<div class="col-lg-6 col-md-6 col-12 p-6 bg-light">
+			<div class="col-lg-6 col-12 p-6 bg-light">
 				<div class="mb-5">
 					@include('components.section-title', ['section' => 'WHY CHOOSE US', 'title' => 'HR Solutions that Grow with Your Business'])
 				</div>
