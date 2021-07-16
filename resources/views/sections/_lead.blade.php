eed4036cde8ee82e956d88ea4a1d38f51a849f79
Arthur Villar <arthur@iMac.local>
2021-07-16 10:39:24 -0400

    Test connection

--- resources/views/sections/_lead.blade.php
+++ resources/views/sections/_lead.blade.php
@@ -5,7 +5,7 @@
 			<div class="col-12 text-white">
 				<div class="mb-4">
 					<h5 class="highlight-thin m-0">GET YOUR JOB DONE!</h5>
-					<h1 class="highlight-bold" style="font-size: 400%">Recruiting Experts in NYC</h1>
+					<h1 class="highlight-bold" style="font-size: 400%">Recruiting Expert in NYC</h1>
 				</div>
 				<div>
 					@include('components.cta')
